db71cf70ffd67d087f565bc7067f499479dedebc
Ermir <ermirjace0@gmail.com>
2020-06-04 15:48:07 +0200

    commit

--- index.js
+++ index.js
@@ -1,33 +1,27 @@
 const express = require('express')
 const path = require('path')
 const PORT = process.env.PORT || 5000
+const { Pool } = require('pg');
+const pool = new Pool({
+  connectionString: process.env.DATABASE_URL,
+  ssl: {
+    rejectUnauthorized: false
+  }
+});
 
 express()
   .use(express.static(path.join(__dirname, 'public')))
   .set('views', path.join(__dirname, 'views'))
   .set('view engine', 'ejs')
-  .get('/', (req, res) => res.render('pages/index'))
+  .get('/', async (req, res) => {
+    try {
+      const client = await pool.connect();
+      const result = await client.query('SELECT * FROM salesforce');
+      const results = { 'results': (result) ? result.rows : null};
+      res.render('pages/db', results );
+      client.release();
+    } catch (err) {
+      console.error(err);
+      res.send("Error " + err);
+    }})
   .listen(PORT, () => console.log(`Listening on ${ PORT }`))
-
-  
-
-const { Client } = require('pg'); 
-express().get('/db', (req, res) => {
-      var dbOpts = {
-        connectionString: process.env.DATABASE_URL, 
-        ssl : true
-      }
-    const client = new Client(dbOpts);    
-    client.connect();    
-    client.query('SELECT id, name, Description__c FROM salesforce.example__c;', (err, dbRes) => {
-        if (err) throw err;     
-
-        res.render('pages/db',{
-          results : dbRes.rows
-        });
-
-        client.end();
-      });
-
-    client.end();   
-})
